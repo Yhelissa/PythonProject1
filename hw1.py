4b528891b4c760f58bf7e554e673c9b5f9cc0d66
Ashley Bercy <yhelissa79@gmail.com>
2025-09-24 10:14:41 -0400

    AshleyB's homework2

--- hw1.py
+++ hw1.py
@@ -9,14 +9,14 @@ for line in lines_in_file[1:]:
     line=line.strip()
     split_line=line.split("\t")
     name=split_line[1]
-    year=int(split_line[3])
-    na_sales=split_line[6]
-    eur_sales=split_line[7]
-    jp_sales=split_line[8]
+    year=float(split_line[3])
+    na_sales=float(split_line[6])
+    eur_sales=float(split_line[7])
+    jp_sales=float(split_line[8])
     other_sales=split_line[9]
     global_sales=float(split_line[10])
     years_since_release = int(current_year - year)
-    avg_sales=(global_sales * years_since_release)/4
+    avg_sales=(na_sales + eur_sales + jp_sales * years_since_release)/3
     formatted_avg_sales=f"{avg_sales:.2f}"
-    print("the video game ", name ,"had an average yearly sale of:",formatted_avg_sales ," copies sold since its release")
+    print("the video game:\n ", name ,"sold: ",formatted_avg_sales ," millions of copies on average since its release")
 print("all done")
